ad91beaab044eca39f5a20e8de43d0f733ff12bb
thekiba <hr@thekiba.io>
2023-11-17 03:03:51 +0400

    chore: refine configuration for dev/prod modes and data consistency

--- webpack.config.js
+++ webpack.config.js
@@ -3,57 +3,63 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
 
-module.exports = {
-  entry: './src/index.ts',
-  module: {
-    rules: [
-      {
-        test: /\.ts?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(png|svg|fft)$/,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.html$/i,
-        loader: 'html-loader',
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: "assets/[hash][ext][query]",
-    clean: true,
-  },
-  devServer: {
-    static: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 4000,
-    allowedHosts: 'all',
-  },
-  resolve: {
-    fallback: {
-      buffer: require.resolve('buffer/'),
-    },
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      Buffer: ['buffer', 'Buffer'],
-    }),
-    new CopyPlugin({
-      patterns: [
-        { from: "assets", to: "assets" },
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.ts?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(png|svg|fft)$/,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.html$/i,
+          loader: 'html-loader',
+        },
       ],
-    }),
-    new HtmlWebpackPlugin({
-      template: './index.html',
-    }),
-  ],
-  mode: 'production',
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      assetModuleFilename: "assets/[hash][ext][query]",
+      clean: true,
+    },
+    devServer: {
+      static: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 4000,
+      allowedHosts: 'all',
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+      fallback: {
+        buffer: require.resolve('buffer/'),
+      }
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(isProduction ? 'production' : 'development')
+      }),
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+      }),
+      new CopyPlugin({
+        patterns: [
+          {from: "assets", to: "assets"},
+        ],
+      }),
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      })
+    ],
+    mode: 'production',
+  };
 };
